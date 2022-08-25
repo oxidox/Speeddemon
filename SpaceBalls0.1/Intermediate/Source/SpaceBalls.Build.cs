abd8b44f4568666012ca4924e194d41ea58c7382
oxidox <henrik.arlan@gmail.com>
2022-08-25 17:11:50 +0200

    Tried to improve cleint jitters, and fiddeled with the physics framerate + some other shit

---
+++ SpaceBalls0.1/Intermediate/Source/SpaceBalls.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class SpaceBalls : ModuleRules
+{
+	public SpaceBalls(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
